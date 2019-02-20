e8e15ea64577ed67cd25073ebf8bb12655fdb420
Antti Ahonen <Antti.Ahonen@mandatumlife.fi>
2019-02-20 09:02:31 +0200

    Added bad curlybraces checker test example

---
+++ src/test/java/fi/aalto/testingandqa/algorithm/BadCurlyBracesCheckerTest.java
@@ -0,0 +1,33 @@
+package fi.aalto.testingandqa.algorithm;
+
+import static fi.aalto.testingandqa.algorithm.CurlyBracesChecker.hasValidCurlyBraces;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BadCurlyBracesCheckerTest {
+
+    @Test
+    public void testCurlyBraces() {
+        String code = "this.code = variable;";
+        assertTrue(hasValidCurlyBraces(code));
+        code = "public class JavaClass {}";
+        assertTrue(hasValidCurlyBraces(code));
+        code = "public class JavaClass {";
+        assertFalse(hasValidCurlyBraces(code));
+        code = "public class JavaClass }";
+        assertFalse(hasValidCurlyBraces(code));
+        code = "public class JavaClass {" +
+                "   JavaClass() { }" +
+                "}";
+        assertTrue(hasValidCurlyBraces(code));
+
+        try {
+            hasValidCurlyBraces(null);
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+    }
+
+}
